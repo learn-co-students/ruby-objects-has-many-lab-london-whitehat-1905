90753876e344bf079eb2dada74d84d06103da180
jnrichards <joelrichards80@gmail.com>
2019-05-08 12:03:49 +0100

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,20 @@
+class Song
+  attr_accessor :name, :artist
+  @@all = []
+  def initialize(name)
+    @name = name
+    @@all << self
+    @artist = artist
+  end
+
+  def artist_name
+    if self.artist
+      self.artist.name
+    else
+      nil
+    end
+  end
+  def self.all
+    @@all
+  end
+end
